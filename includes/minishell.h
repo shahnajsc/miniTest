e2d53d066eea490196657335c2cc255b6b354096
shahnajsc <sschowdhury29@gmail.com>
2025-01-28 11:04:45 +0200

    makefile and header update

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,7 +1,19 @@
 #ifndef MINISHELL.H
 # define MINISHELL.H
 
+//required library
+// fcntl: file handle
+// sys/wait: process wait(waitpid)
+// stdio: perror
+// errno: strerrno
+# include "../libft/includes/libft.h"
+# include <fcntl.h>
+# include <sys/wait.h>
 # include <stdio.h>
-# include <unistd.h>
+# include <errno.h>
+# include <string.h>
+# include <stdlib.h>
+
+//FUNCTIONS
 
 #endif
